31625f1b5977fb7ec14012838162686d493faeec
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-22 22:55:29 +0200

    Doodgy draggable player

---
+++ Assets/Scripts/DragMe.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragMe : MonoBehaviour {
+
+    // http://k2nicestudio.com/tutorial/2016/1/31/tutorial-1-click-or-touch-to-drag-an-object
+    // Siglhy modified to use rigidbody2d...
+    //This code is for 2D click/drag gameobject
+    //Please make sure to change Camera Projection to Orthographic
+    //Add Collider (not 2DCollider) to gameObject  
+
+    public GameObject gameObjectTodrag; //refer to GO that being dragged
+
+    public Vector3 GOcenter; //gameobjectcenter
+    public Vector3 touchPosition; //touch or click position
+    public Vector3 offset;//vector between touchpoint/mouseclick to object center
+    public Vector3 newGOCenter; //new center of gameObject
+
+    RaycastHit hit; //store hit object information
+
+    public bool draggingMode = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+  
+
+        //***********************
+        // *** TOUCH TO DRAG ****
+        //***********************
+        foreach (Touch touch in Input.touches)
+        {
+            switch (touch.phase)
+            {
+                //When just touch
+                case TouchPhase.Began:
+                    //convert mouse click position to a ray
+                    //Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);// Changed to conserv 2d stuff related like rigidbody2d
+
+                    //if ray hit a Collider ( not 2DCollider)
+                    // if (Physics.Raycast(ray, out hit))
+                    if (hit.collider != null)
+                    {
+                        gameObjectTodrag = hit.collider.gameObject;
+                        if (gameObjectTodrag.tag == "Player")
+                        {
+                            GOcenter = gameObjectTodrag.transform.position;
+                            touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                            offset = touchPosition - GOcenter;
+                            draggingMode = true;
+                        }
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (draggingMode)
+                    {
+                        touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                        newGOCenter = touchPosition - offset;
+                        gameObjectTodrag.transform.position = new Vector3(newGOCenter.x, newGOCenter.y, GOcenter.z);
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    draggingMode = false;
+                    break;
+            }
+        }
+    }
+}
